1feef9d03bda2fa44be07bcf0dc85ae31c25e4d4
jinnyfruit <68141544+jinnyfruit@users.noreply.github.com>
2023-03-27 13:43:46 +0900

    update code

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# 샘플 Python 스크립트입니다.
+from typing import List
+from konlpy.tag import Okt
+from typing import List
+from lexrankr import LexRank
+import json
 
-# ⌃R을(를) 눌러 실행하거나 내 코드로 바꿉니다.
-# 클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 ⇧ 두 번을(를) 누릅니다.
+#한국어 추출요약
+class OktTokenizer:
+    okt: Okt = Okt()
+    def __call__(self, text: str) -> List[str]:
+        tokens: List[str] = self.okt.pos(text, norm=True, stem=True, join=True)
+        return tokens
+
+def Summerization() :
+    # 1. init using Okt tokenizer
+    mytokenizer: OktTokenizer = OktTokenizer()
+    lexrank: LexRank = LexRank(mytokenizer)
+    text = ""
+
+    # 2. summarize (like, pre-computation)
+    lexrank.summarize(text)
+
+    summerization = []
+
+    # 3. probe (like, query-time)
+    summaries: List[str] = lexrank.probe()
+    for summary in summaries:
+        summerization.append(summary)
+
+    return summerization
 
 
-def print_hi(name):
-    # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
-    print(f'Hi, {name}')  # 중단점을 전환하려면 ⌘F8을(를) 누릅니다.
 
-
-# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
